4759af551f619b064ed82c49ae18d3f3e8e2b19e
Paulo Henrique <paulo.15.paulo@gmail.com>
2018-09-15 18:43:40 -0300

    fix url

--- src/main/java/com/algartech/hacka/chatbot/controller/MailingController.java
+++ src/main/java/com/algartech/hacka/chatbot/controller/MailingController.java
@@ -66,7 +66,7 @@ public class MailingController {
         new SmsService().sendSms(sms.getPhone(), sms.getCpf(), sms.getTexto());
     }
 
-    @RequestMapping(value = "/buscaDividas/{cpf}", method = RequestMethod.GET)
+    @RequestMapping(value = "/dividas/{cpf}", method = RequestMethod.GET)
     public String findByCPF(@PathVariable("cpf") String cpf){
         return  service.findConsolidatedByCPF(cpf);
     };
