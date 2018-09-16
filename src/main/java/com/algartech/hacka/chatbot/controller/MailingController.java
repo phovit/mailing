e2a51d8d2617477e96e0bc62fba57d3cd61a1878
Paulo Henrique <paulo.15.paulo@gmail.com>
2018-09-15 21:06:49 -0300

    liogs

--- src/main/java/com/algartech/hacka/chatbot/controller/MailingController.java
+++ src/main/java/com/algartech/hacka/chatbot/controller/MailingController.java
@@ -8,6 +8,8 @@ import com.algartech.hacka.chatbot.model.SmsDTO;
 import com.algartech.hacka.chatbot.service.EmailService;
 import com.algartech.hacka.chatbot.service.MailingService;
 import com.algartech.hacka.chatbot.service.SmsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +26,9 @@ import java.util.List;
 @RestController
 @RequestMapping("mailing")
 public class MailingController {
+
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private Double valorRetorno = 1563.64D;
 
     @Autowired
@@ -84,11 +89,13 @@ public class MailingController {
 
     @RequestMapping(value = "/setTypeNegotiation", method = RequestMethod.POST)
     public ResponseDTO tipoNegociacao(@RequestBody GenericRequestDTO view) {
+        logger.info("setTypeNegotiation-->"+view.getValor()+" string-->"+view.getString());
         return new ResponseDTO("Vai ser moleza! Para darmos seguimento na nossa negociação, qual o valor ideal para a entrada?");
     }
 
     @RequestMapping(value = "/sendValueIncome", method = RequestMethod.POST)
     public ResponseDTO valorEntrada(@RequestBody GenericRequestDTO view) {
+        logger.info("sendValueIncome-->"+view.getValor()+" string-->"+view.getString());
 
         Double valor = valorRetorno -  view.getValor();
         return new ResponseDTO("Entendi! O saldo da conta é R$"+valor+".  E agora qual é o valor ideal para as parcelas? Lembrando que o valor mínimo é de 20 reais!");
@@ -96,6 +103,7 @@ public class MailingController {
 
     @RequestMapping(value = "/sendValueParc", method = RequestMethod.POST)
     public ResponseDTO valorParcelas(@RequestBody GenericRequestDTO view) {
+        logger.info("sendValueParc-->"+view.getValor()+" string-->"+view.getString());
         String retorno = "";
         Double saldo = valorRetorno %  view.getValor();
 
@@ -116,20 +124,23 @@ public class MailingController {
 
     @RequestMapping(value = "/sendAccept", method = RequestMethod.POST)
     public ResponseDTO aceite(@RequestBody GenericRequestDTO view) {
+        logger.info("sendAccept-->"+view.getValor()+" string-->"+view.getString());
 
         return new ResponseDTO("Que bom! Qual é a melhor data para iniciar a pagar?");
     }
 
     @RequestMapping(value = "/sendDataPayment", method = RequestMethod.POST)
     public ResponseDTO dataNegociacao(@RequestBody GenericRequestDTO view) {
+        logger.info("sendDataPayment-->"+view.getValor()+" string-->"+view.getString());
 
         return new ResponseDTO("Deseja receber os códigos de barras por SMS?");
     }
 
-
+git add
 
     @RequestMapping(value = "/sendSms", method = RequestMethod.POST)
     public ResponseDTO enviaSms(@RequestBody GenericRequestDTO view) {
+        logger.info("sendSms-->"+view.getValor()+" string-->"+view.getString());
 
         new SmsService().sendSms("34988657231", "10700342664", "00000.00000. 00000.000000 00000.000000 0 00000000000000");
         return new ResponseDTO("Perfeito! Muito Obrigado!");
